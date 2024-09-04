f20c697f3f0418fe21648f538914f71066c3d210
Dev. Siamul Hassan <syeam45@gmail.com>
2024-09-04 11:06:38 +0600

    sticky nav added

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "../ui/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../ui/Footer";
 import NavBar from "../ui/NavBar";
 
 const RootLayout = () => {
+  const menuRef = useRef(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY);
+      if (window.scrollY > 205) {
+        menuRef.current.classList.add(
+          "fixed",
+          "top-0",
+          "w-full",
+          "shadow-lg",
+          "z-[99]",
+          "transition-all",
+          "duration-300"
+        );
+      } else {
+        menuRef.current.classList.remove(
+          "fixed",
+          "top-0",
+          "w-full",
+          "shadow-lg",
+          "z-[99]"
+        );
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-      <NavBar />
+      <div ref={menuRef}>
+        <Header />
+        <NavBar />
+      </div>
       <div className="grow bg-bg_FF6">
         <Outlet />
       </div>
